56ed8f9de3ca9c4aa09fd35ae231a36c7a160f01
manchandajayant <maanjayant@gmail.com>
2020-09-26 18:37:55 +0530

    infinite scroll added

--- script.js
+++ script.js
@@ -2,7 +2,23 @@ var accessKey = "YCGAlL3u9atOQlCUNh77ecjUYktCjCotWtZDpgieVH8";
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 var photosArray = [];
+
+//check function for loading
+
+function imageLoaded() {
+  console.log("imageloaded");
+  imagesLoaded++;
+  console.log(imagesLoaded);
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+    console.log(ready);
+  }
+}
 
 function setAttributes(element, attributes) {
   for (const key in attributes) {
@@ -11,6 +27,8 @@ function setAttributes(element, attributes) {
 }
 
 function displayPhotos() {
+  totalImages = photosArray.length;
+  imagesLoaded = 0;
   photosArray.forEach((photo) => {
     //create <a> to link to unsplash
     const item = document.createElement("a");
@@ -31,9 +49,12 @@ function displayPhotos() {
       alt: photo.alt_description,
       title: photo.alt_description,
     });
+    // check when images are loaded
+    img.addEventListener("load", imageLoaded);
+
+    //run
     item.appendChild(img);
     imageContainer.appendChild(item);
-    console.log(photo);
   });
 }
 async function getImagesFromAPi() {
@@ -42,7 +63,9 @@ async function getImagesFromAPi() {
   try {
     const result = await fetch(imageFetchUrl);
     photosArray = await result.json();
-    console.log(photosArray);
+    console.log("loaded", imagesLoaded);
+    console.log("total", totalImages);
+
     displayPhotos();
     // for (var i = 0; i < data.length; i++) {
     //   imageContainer.innerHTML = data[i].urls.raw;
@@ -52,5 +75,17 @@ async function getImagesFromAPi() {
     console.log("nada", error);
   }
 }
+
+// check to see scrolling at the bottom
+
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getImagesFromAPi();
+  }
+});
 
 getImagesFromAPi();
